60964f640ee9799315ce12efd33a082ab4ff7e50
Santhosh Babu <html5webster@gmail.com>
2014-01-12 20:46:26 +0530

    updated header.php

--- project-uifront/WebContent/i/template/php-template/includes/header.php
+++ project-uifront/WebContent/i/template/php-template/includes/header.php
@@ -1,6 +1,8 @@
 
   <header class="site-header">
-  <!-- site-header begin -->
-    <?php include "navigation.php"; ?>
+	<!-- site-header begin -->
+    <div class="container">
+      <?php include "navigation.php"; ?>
+	  </div>
   <!-- site-header end -->  
   </header>
