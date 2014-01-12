fe7299354a99cded5631ea35062e09f1411880c6
Santhosh Babu <html5webster@gmail.com>
2014-01-12 20:51:59 +0530

    updated brendan-eich document

--- project-uifront/WebContent/i/dev-env/brendan-eich/content.php
+++ project-uifront/WebContent/i/dev-env/brendan-eich/content.php
@@ -1,15 +1,188 @@
-<div class="row">
-  <div class="span6">
+<div class="about">
+  <div class="container">
+    <h2 class="title">
+      About
+    </h2>
+    <div class="row">
+      <div class="col-xs-6 col-md-8">
+        <div class="left-panel">
+          <div class="row">
+            <div class="col-sm-6 hidden-xs">
+              Name
+            </div>
+            <div class="col-sm-6">
+              Brendan Eich
+            </div>
+            <div class="col-sm-6 hidden-xs">
+              Born
+            </div>
+            <div class="col-sm-6">
+              1961
+            </div>
+            
+            <div class="col-sm-6 hidden-xs">
+              Credits
+            </div>
+            <div class="col-sm-6">
+              Developed JavaScript
+            </div>
+            <div class="col-sm-6 hidden-xs">
+              Current
+            </div>
+            <div class="col-sm-6">
+              CTO at Mozilla Corporation
+            </div>
+            <div class="col-sm-6 hidden-xs">
+              Website
+            </div>
+            <div class="col-sm-6">
+              <a href="http://brendaneich.com/">
+                brendaneich.com/
+              </a>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-6 col-md-4">
+        <div class="right-panel">
+          <figure>
+            <span data-picture data-alt="Brendan Eich">
+              <span data-src="<?php echo $pageAsset;?>/image/small/Brendan-Eich.jpg">
+              </span>
+              <span data-src="<?php echo $pageAsset;?>/image/medium/Brendan-Eich.jpg" data-media="(min-width: 640px)">
+              </span>
+              <span data-src="<?php echo $pageAsset;?>/image/large/Brendan-Eich.jpg" data-media="(min-width: 1024px)">
+              </span>
+              <!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+              <noscript>
+                <img src="<?php echo $pageAsset;?>/image/small/Brendan-Eich.jpg" alt="Brendan Eich">
+              </noscript>
+            </span>
+          </figure>
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="span6">
-    <figure>
-      <span data-picture data-alt="Brendan Eich">
-        <span data-src="<?php echo $pageAsset;?>/image/small/Brendan-Eich.jpg"></span>
-        <span data-src="<?php echo $pageAsset;?>/image/medium/Brendan-Eich.jpg" data-media="(min-width: 640px)"></span>
-        <span data-src="<?php echo $pageAsset;?>/image/large/Brendan-Eich.jpg" data-media="(min-width: 1024px)"></span>
-        <!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
-        <noscript><img src="<?php echo $pageAsset;?>/image/small/Brendan-Eich.jpg" alt="Brendan Eich"></noscript>
-      </span>
-</figure>
+</div>
+<div class="work">
+  <div class="container">
+    <h2 class="title">
+      Work
+    </h2>
+    <div class="row">
+      <div class="col-xs-12">
+        <p>
+          Brendan Eich invented JavaScript in 10 days in May 1995, which has now become the Internet's most widely used programming language.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="videos">
+  <div class="container">
+    <h2 class="title">
+      Video(s)
+    </h2>
+    <div class="row">
+      <div class="vid">
+        <div class="col-md-6  col-xs-12">
+          <iframe class="v" width="640" height="360" src="//www.youtube.com/embed/IXIkTrq3Rgg" frameborder="0" allowfullscreen>
+          </iframe>
+        </div>
+      </div>
+      <div class="desc">
+        <div class="col-md-4  col-xs-8">
+          <div class="name">
+            JS Responsibilities
+          </div>
+          <ul class="topics">
+            <li>
+              ES6, ES7
+            </li>
+            <li>
+              Value Objects
+            </li>
+            <li>
+              Overloadable Operators
+            </li>
+            <li>
+              Cacheable Multimethods
+            </li>
+            <li>
+              SIMD
+            </li>
+            <li>
+              asm.js
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="download">
+        <div class="col-md-2  col-xs-4">
+          <div class="name">
+            Download
+          </div>
+          <ul class="d">
+            <li>
+              <a href="#">
+                MP4 173MB
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                FLV 83MB
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="clearfix visible-md visible-lg">
+      </div>
+      
+      <div class="vid">
+        <div class="col-md-6 col-xs-12">
+          <iframe class="v" width="640" height="360" src="//www.youtube-nocookie.com/embed/wPz0bRQmaPc" frameborder="0" allowfullscreen>
+          </iframe>
+        </div>
+      </div>
+      <div class="desc">
+        <div class="col-md-4  col-xs-8">
+          <div class="name">
+            JavaScript at 18: Legal to Gamble
+          </div>
+          <ul class="topics">
+            <li>
+              ECMAScript Harmony ( ES6/7 )
+            </li>
+            <li>
+              Improving Browser Support
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="download">
+        <div class="col-md-2  col-xs-4">
+          <div class="name">
+            Download
+          </div>
+          <ul class="d">
+            <li>
+              <a href="#">
+                MP4 173MB
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                FLV 83MB
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+    </div>
   </div>
 </div>
