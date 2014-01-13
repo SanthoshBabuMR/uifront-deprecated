8b04f47af05e761fe90123eaa46f13617ddd9a6d
santhosh <html5webster@gmail.com>
2014-01-13 22:55:19 +0530

    updated download links for brendan-eich document

--- project-uifront/WebContent/i/dev-env/brendan-eich/content.php
+++ project-uifront/WebContent/i/dev-env/brendan-eich/content.php
@@ -150,13 +150,18 @@
 			          </div>
 			          <ul class="d">
 			            <li>
-			              <a href="#">
-			                MP4 173MB
+			              <a href="http://r6---sn-h557snez.googlevideo.com/videoplayback?ratebypass=yes&ipbits=0&upn=Ar_ZIt7ZLbw&fexp=900064%2C901454%2C924616%2C938630%2C936910%2C936913%2C907231%2C907240%2C921090&ms=au&mt=1389633472&pcm2fr=yes&source=youtube&key=yt5&itag=22&mv=m&expire=1389659136&sparams=id%2Cip%2Cipbits%2Citag%2Cpcm2fr%2Cratebypass%2Csource%2Cupn%2Cexpire&ip=117.193.203.59&sver=3&id=2172244ebab74608&signature=7023C42DBFB74F0450A9A1823B28C76D4E773155.838378DE80DC349345CFB810292BE25378EE9E9A&title=Brendan%20Eich%3A%20JS%20Responsibilities%20--%20JSConf%20EU%202013">
+			                MP4 - 274.5 MB
 			              </a>
 			            </li>
 			            <li>
-			              <a href="#">
-			                FLV 83MB
+			              <a href="http://r6---sn-h557snez.googlevideo.com/videoplayback?ratebypass=yes&ipbits=0&upn=Ar_ZIt7ZLbw&fexp=900064%2C901454%2C924616%2C938630%2C936910%2C936913%2C907231%2C907240%2C921090&ms=au&mt=1389633472&pcm2fr=yes&source=youtube&key=yt5&itag=18&mv=m&expire=1389659136&sparams=id%2Cip%2Cipbits%2Citag%2Cpcm2fr%2Cratebypass%2Csource%2Cupn%2Cexpire&ip=117.193.203.59&sver=3&id=2172244ebab74608&signature=4362550C7A2C9BB48663C3D0347235B38B42BAC1.867FED6DB84612FEF3AEC1F5E5F7DC4EA8334B6E&title=Brendan%20Eich%3A%20JS%20Responsibilities%20--%20JSConf%20EU%202013">
+			                MP4 - 79.6 MB
+			              </a>
+			            </li>
+						<li>
+			              <a href="http://r6---sn-h557snez.googlevideo.com/videoplayback?ratebypass=yes&ipbits=0&upn=Ar_ZIt7ZLbw&fexp=900064%2C901454%2C924616%2C938630%2C936910%2C936913%2C907231%2C907240%2C921090&ms=au&mt=1389633472&pcm2fr=yes&source=youtube&key=yt5&itag=43&mv=m&expire=1389659136&sparams=id%2Cip%2Cipbits%2Citag%2Cpcm2fr%2Cratebypass%2Csource%2Cupn%2Cexpire&ip=117.193.203.59&sver=3&id=2172244ebab74608&signature=983CA26D74A125BFA6ADDF65BC540F11D795A1FD.07C9F78102903913A1FD2A73F8674684EEB5AD9B&title=Brendan%20Eich%3A%20JS%20Responsibilities%20--%20JSConf%20EU%202013">
+			                WEBM - 67.4 MB
 			              </a>
 			            </li>
 			          </ul>
@@ -206,14 +211,19 @@
 			            Download
 			          </div>
 			          <ul class="d">
-			            <li>
-			              <a href="#">
-			                MP4 173MB
+			             <li>
+			              <a href="http://r3---sn-h557sn7y.googlevideo.com/videoplayback?id=c0fcf46d142668f7&upn=lHETbIQssNw&mt=1389633643&sparams=id%2Cip%2Cipbits%2Citag%2Cpcm2fr%2Cratebypass%2Csource%2Cupn%2Cexpire&mv=m&fexp=900064%2C901454%2C924616%2C938630%2C936910%2C936913%2C907231%2C907240%2C921090&pcm2fr=yes&expire=1389658867&sver=3&itag=22&ipbits=0&ratebypass=yes&ms=au&ip=117.193.203.59&key=yt5&source=youtube&signature=377BF40E5289BF64F8D163D06B0634270546E7FC.B597542430FA28570CA42F349122C760454AB432&title=Fluent%202013%20-%20Brendan%20Eich%2C%20%27JavaScript%20at%2018%20-%20Legal%20to%20Gamble%27">
+			                MP4 - 265 MB
 			              </a>
 			            </li>
 			            <li>
-			              <a href="#">
-			                FLV 83MB
+			              <a href="http://r3---sn-h557sn7y.googlevideo.com/videoplayback?id=c0fcf46d142668f7&upn=lHETbIQssNw&mt=1389633643&sparams=id%2Cip%2Cipbits%2Citag%2Cpcm2fr%2Cratebypass%2Csource%2Cupn%2Cexpire&mv=m&fexp=900064%2C901454%2C924616%2C938630%2C936910%2C936913%2C907231%2C907240%2C921090&pcm2fr=yes&expire=1389658867&sver=3&itag=18&ipbits=0&ratebypass=yes&ms=au&ip=117.193.203.59&key=yt5&source=youtube&signature=E0D7F9FCC9BFCB2B6A6D5CC0C5EC6FE889D93C02.6DF7D61506518DE570ED8203EBCB4F8461386EF6&title=Fluent%202013%20-%20Brendan%20Eich%2C%20%27JavaScript%20at%2018%20-%20Legal%20to%20Gamble%27">
+			                MP4 - 66.3 MB
+			              </a>
+			            </li>
+						<li>
+			              <a href="http://r3---sn-h557sn7y.googlevideo.com/videoplayback?id=c0fcf46d142668f7&upn=lHETbIQssNw&mt=1389633643&sparams=id%2Cip%2Cipbits%2Citag%2Cpcm2fr%2Cratebypass%2Csource%2Cupn%2Cexpire&mv=m&fexp=900064%2C901454%2C924616%2C938630%2C936910%2C936913%2C907231%2C907240%2C921090&pcm2fr=yes&expire=1389658867&sver=3&itag=43&ipbits=0&ratebypass=yes&ms=au&ip=117.193.203.59&key=yt5&source=youtube&signature=79B69E97558D227B5816690D89AAF1E78AD34E2D.76422844372C1A3C62BEF2E5AAC7768E0A5A4319&title=Fluent%202013%20-%20Brendan%20Eich%2C%20%27JavaScript%20at%2018%20-%20Legal%20to%20Gamble%27">
+			                WEBM - 91.6 MB
 			              </a>
 			            </li>
 			          </ul>
